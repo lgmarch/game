9a7963549c0e0d1f14afeef048aec881cfc3411a
Lyubov <lmarch@yandex.ru>
2020-03-03 03:31:05 +0300

    Домашняя работа. Работа над ошибками. Урок 1.

--- core/src/com/lmarch/rpg/game/GeekRpgGame.java
+++ core/src/com/lmarch/rpg/game/GeekRpgGame.java
@@ -5,25 +5,17 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 
 public class GeekRpgGame extends ApplicationAdapter {
 	private SpriteBatch batch; //
 	private Texture textureGrass;
-	private Texture texturePointer;
 	private Hero hero;
-
-	private Vector2 pointerPosition;
-	private float rt;
-
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		hero = new Hero();
 		textureGrass = new Texture("grass.png");
-		texturePointer = new Texture("pointer.png");
-		pointerPosition = new Vector2(0, 0);
 	}
 
 	@Override
@@ -34,7 +26,7 @@ public class GeekRpgGame extends ApplicationAdapter {
 		update(dt);
 
 		//Цвет очистки экрана: выбор цвета
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		//Очистка экрана
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -45,22 +37,13 @@ public class GeekRpgGame extends ApplicationAdapter {
 				batch.draw(textureGrass, i * 80, j * 80);
 			}
 		}
-		batch.draw(texturePointer, pointerPosition.x-32, pointerPosition.y-32,
-				32, 32, 64, 64, 1, 1, rt,
-				0, 0, 64, 64, false, false);
 		hero.render(batch);
 
 		batch.end();
 	}
 
 	public void update(float dt){
-		rt -= dt * 90.0f;
 		hero.update(dt);
-
-		if (Gdx.input.justTouched()){
-			pointerPosition.set(Gdx.input.getX(), 720.0f - Gdx.input.getY());
-		}
-		hero.moveToMouse(pointerPosition, dt);
 	}
 	
 	@Override
--- core/src/com/lmarch/rpg/game/Hero.java
+++ core/src/com/lmarch/rpg/game/Hero.java
@@ -1,70 +1,58 @@
 package com.lmarch.rpg.game;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Hero {
     private Texture texture;
-    private Vector2 position;
+    private Texture texturePointer;
+    private Vector2 position; //Позиция героя
+    private Vector2 dst; //Полиция поинтера
+
     private float speed;
+    private float lifeTime;
     private float param;
 
     public Hero(){
         this.texture = new Texture("pig1.png");
-        this.position = new Vector2(200, 200);
+        texturePointer = new Texture("pointer.png");
+        this.position = new Vector2(100, 100);
+        this.dst = new Vector2(position);
         this.speed = 100.0f;
         this.param = 1.0f;
     }
 
     //Прорисовка
     public void render(SpriteBatch batch){
+        batch.draw(texturePointer, dst.x - 32, dst.y - 32,
+                32, 32, 64,64,
+                1, 1, lifeTime * 90.0f, 0, 0, 64, 64, false, false);
+
         batch.draw(texture, position.x - 32, position.y - 32,
                 32, 32, 64,64,
                 1, 1, param, 0, 0, 64, 64, false, false);
-
-//        batch.draw(texture, position.x, position.y, texture.getWidth()/2, texture.getHeight()/2,
-//                texture.getWidth(),texture.getHeight(), param, param, 0, 0, 0,
-//                600, 500, false, false);
     }
 
     //логика движения персонажа - расчет
     public void update(float dt){
+        lifeTime +=dt;
+        if (Gdx.input.justTouched()){
+            dst.set(Gdx.input.getX(), 720 - Gdx.input.getY());
+        }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.A)){
+        if (position.x > dst.x){
             position.x -= speed * dt;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.D)){
+        if (position.x < dst.x){
             position.x += speed * dt;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.S)){
+        if (position.y > dst.y){
             position.y -= speed * dt;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.W)){
+        if (position.y < dst.y){
             position.y += speed * dt;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.E)){
-            param += dt * 90.0f;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.Q)){
-            param -= dt * 90.0f;
-        }
-    }
-
-    public void moveToMouse(Vector2 pointerPosition, float dt){
-//        position.x = pointerPosition.x;
-//        position.y = pointerPosition.y;
-
-        if (position.x <= pointerPosition.x)
-            position.x += speed * dt;
-        if (position.x >= pointerPosition.x)
-            position.x -= speed * dt;
-
-        if (position.y <= pointerPosition.y)
-            position.y += speed * dt;
-        if (position.y >= pointerPosition.y)
-            position.y -= speed * dt;
     }
 }
